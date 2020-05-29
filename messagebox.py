e45ac7393b9169431a73f5f45bce915f79a4f3f3
issamelferkh <issam.elferkh@gmail.com>
2020-05-29 12:18:26 +0000

    finish GUI and Display Data in messagebox

---
+++ messagebox.py
@@ -0,0 +1,13 @@
+import tkinter.messagebox as msg
+# import traceback
+# from tkinter import *
+
+# try:
+#     1/0
+# except Exception as error:
+#     tm.showerror(title="Error",
+#                  message="An error has occurred: '" + str(error) + "'.",
+#                  detail=traceback.format_exc())
+
+msg.showinfo(title=None, 
+            message="You send the ID:")
--- trans_msg.py
+++ trans_msg.py
@@ -1,36 +1,61 @@
 from tkinter import *
-from tkinter import messagebox
+import tkinter.messagebox as msg
 
-window = Tk() #setup the app object by calling the Tk() function
+window = Tk()
 
-def if_send():
-    label_id = Label(window, text="sss:(hex)", font=("arial", 9, "bold"))
-    label_id.place(x=50, y=50)
+def show_answer():
+    ID = ID_input.get()
+    Lenght = Lenght_input.get()
+    Cycle_time = Cycle_time_input.get()
+    Data = Data1_input.get()+Data2_input.get()+Data3_input.get()+Data4_input.get()+Data5_input.get()+Data6_input.get()+Data7_input.get()+Data8_input.get()
+    msg.showinfo(title=None, 
+                message="You send:\nID: '"+ID+"'\nLenght: '"+Lenght+"'\nCycle time: '"+Cycle_time+"'\nData: '"+Data+"'")
 
-# ID Label
-label_id = Label(window, text="ID:(hex)", font=("arial", 9, "bold"))
-label_id.place(x=10, y=10)
+# ID:(hex)
+Label(window, text = "ID:(hex)", font=("arial", 9, "bold")).grid(row=0, column=0)
+ID_input = Entry(window, width=10)
+ID_input.place(x=0, y=20)
 
-# ID Input
-entry_id = Entry(width=10)
-entry_id.place(x=10, y=30)
+# Lenght
+Label(window, text="Lenght:", font=("arial", 9, "bold")).place(x=180, y=0)
+Lenght_input = Entry(window, width=9)
+Lenght_input.place(x=180, y=20)
 
-# ID Output
-label_id = Label(window, text="ID:(hex)", font=("arial", 9, "bold"))
-label_id.place(x=10, y=10)
+# Cycle time
+Label(window, text="cycle time", font=("arial", 9, "bold")).place(x=0, y=40)
+Label(window, text="ms", font=("arial", 6, "bold")).place(x=80, y=60)
+Cycle_time_input = Entry(window,width=10)
+Cycle_time_input.place(x=0,y=60)
 
+# Data:(hex)
+Label(window, text="Data:(hex)", font=("arial", 9, "bold")).place(x=300, y=0)
+Data1_input = Entry(window, width=2)
+Data1_input.place(x=300, y=20)
+Data2_input = Entry(window, width=2)
+Data2_input.place(x=320, y=20)
+Data3_input = Entry(window, width=2)
+Data3_input.place(x=340, y=20)
+Data4_input = Entry(window, width=2)
+Data4_input.place(x=360, y=20)
+Data5_input = Entry(window, width=2)
+Data5_input.place(x=380, y=20)
+Data6_input = Entry(window, width=2)
+Data6_input.place(x=400, y=20)
+Data7_input = Entry(window, width=2)
+Data7_input.place(x=420, y=20)
+Data8_input = Entry(window, width=2)
+Data8_input.place(x=440, y=20)
 
-# Send Button
-btn_ok = Button(window, text="OK", width=12, bg='white', fg='black', command=if_send)
-btn_ok.place(x=150, y=200)
+# Message Type
+Checkbutton(window, text="send extended ", font=("arial", 9, "bold")).place(x=200, y=40)
+Checkbutton(window, text="send ", font=("arial", 9, "bold")).place(x=200, y=60)
 
-# Cancel Button
-btn_cancel = Button(window, text="cancel", width=12, bg='white', fg='black')
-btn_cancel.place(x=280, y=200)
+# Buttons
+Button(window, text='Send', command=show_answer).place(x=150, y=200)
+Button(window, text='Cancel', command=window.destroy).place(x=280, y=200)
 
 # Window Widgets
-window.title('New Transmit Message')
-window.geometry("500x300+10+20") #window.geometry("width x height + XPOS +Y POS")
+window.geometry("500x300")
+window.title("Transmit message")
 window.mainloop()
-
 
